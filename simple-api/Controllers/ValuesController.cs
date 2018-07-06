f1e70600428f5c0d3a6620ae180eaf2be188d9a5
Ben Pearce <ben.pearce@octopus.com>
2018-07-06 12:09:25 +1000

    initial app commit

---
+++ simple-api/Controllers/ValuesController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace simple_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public Models.Settings Settings { get; }
+
+        public ValuesController(IOptionsSnapshot<Models.Settings> settings)
+        {
+            Settings = settings.Value;
+        }
+        // GET api/values
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return Settings.Value;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ simple-api/Models/Settings.cs
@@ -0,0 +1,9 @@
+namespace simple_api.Models
+{
+    public class Settings 
+    {
+        public string Value { get; set; }
+
+    }
+
+}
